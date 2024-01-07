7f9b1f6b7f90229fa0f6238a71b82bc2b341ae77
DarkSideIT <107682338+DarkSideIT@users.noreply.github.com>
2024-01-08 06:04:37 +0800

    add ApiResources for students

---
+++ myproject/app/Http/Resources/StudentResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StudentResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'age' => $this->age,
+            'university' => $this->university,
+            'faculty' => $this->faculty,
+            'category' => new CategoryResource($this->whenLoaded('category')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at,
+        ];
+    }
+}
