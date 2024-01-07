ba449c054dbd9cb687436721f145122b9bda9076
DarkSideIT <107682338+DarkSideIT@users.noreply.github.com>
2024-01-08 01:17:44 +0800

    add controller for processing routes
    routes: get "/form", post "/form", "/data-table"

---
+++ myproject/app/Http/Controllers/DataController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+
+class DataController extends Controller
+{
+    public function showForm(){
+        return view('form');
+    }
+
+
+    public function processData(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email',
+            'age' => 'required|integer',
+            'univercity' => 'required|string|max:255',
+            'faculty' => 'required|string|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/form')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+
+        $data = $request->all();
+        $fileName = uniqid('data_') . '.json';
+        Storage::put('data/' . $fileName, json_encode($data));
+
+
+        return redirect('/form' -> with('success', 'Data uploaded successfully!'));
+    }
+
+
+    public function showDataTable(){
+        $files = Storage::files('data');
+        $data = [];
+
+
+        foreach ($files as $file) {
+            $data[] = json_decode(Storage::get($file), true);
+        }
+
+
+        return view('data_table', ['data' => $data]);
+    }
+}
